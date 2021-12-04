51c51844c2f68a0e036cc62e8385a807a50d151f
HeroOfElFacil <43790437+HeroOfElFacil@users.noreply.github.com>
2021-12-04 16:00:18 +0100

    resolved

--- scripts/clustering.py
+++ scripts/clustering.py
@@ -36,9 +36,3 @@ if __name__ == "__main__":
     print("Writing data")
     X = np.loadtxt("data/X.csv", delimiter=",")
     user_map = pd.read_csv("data/user_map.csv")
-    X_id = user_map.X_id
-    print("Running clustering...")
-    gen = genieclust.Genie(100, affinity="precomputed", gini_threshold=0.05)
-    labels = gen.fit_predict(X)
-    np.savetxt("../data/labels.csv", labels, delimiter=",", newline="\n")
-    print("Clustering finished...")
