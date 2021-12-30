2826dde4ca60bcdd4a5cf273b350b01f9fb2f327
Kacper Staron <staronkacper99@gmail.com>
2021-12-30 02:43:53 +0100

    complete gui

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
 from flask import Flask, config, render_template, request
+import numpy as np
 
-from scripts import placeholderPlot
+from scripts import placeholderPlot, processing, clustering
 
 app = Flask(__name__)
 
 @app.route('/callback', methods=['POST', 'GET'])
 def cb():
-    return placeholderPlot.gm(request.args.get('data'))
+	data = request.args.get('data')
+
+
+	data = np.array(data.split(';'))
+	weight_dict = {
+			"follow": int(data[4]),
+            "retweet": int(data[5]),
+            "like": int(data[6]),
+            "mention": int(data[7]),
+            "quote": int(data[8]),
+            "reply": int(data[9]),
+            "friend": int(data[10])
+			}
+	us, rel = processing.getDBData() #przekazujemy data[0], todo
+	user_map = processing.createUserMapping(us)
+	X = processing.createX(rel, user_map, weight_dict)
+	X_id = user_map.X_id
+	gen = genieclust.Genie(int(data[1]), affinity="precomputed", gini_threshold=float(data[2]))
+	labels = gen.fit_predict(X)
+
+	if (data[3] == '0'):
+		return placeholderPlot.heatmap(X, labels)
+	else:
+		return placeholderPlot.bubble(labels, int(data[1]))
    
 @app.route('/')
 def index():
-    return render_template('index.html',  graphJSON=placeholderPlot.gm())
+	return render_template('index.html',  graphJSON=placeholderPlot.empty_plot())
--- scripts/placeholderPlot.py
+++ scripts/placeholderPlot.py
@@ -2,13 +2,38 @@ import pandas as pd
 import json
 import plotly
 import plotly.express as px
+import numpy as np
 
-def gm(country='United Kingdom'):
-    df = pd.DataFrame(px.data.gapminder())
+def heatmap(X, labels):
+	X = pd.DataFrame(X)
+	X['labels'] = labels
+	X = X.set_indexes('labels')
+	X.columns = labels
+	X.sort_index(axis=0).sort_index(axis=1)
 
-    fig = px.line(df[df['country']==country], x="year", y="gdpPercap")
-    
-    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-    print(fig.data[0])
-    
-    return graphJSON
+	fig = px.imshow(df)
+	fig.update_yaxes(visible=False, showticklabels=False)
+	fig.update_xaxes(visible=False, showticklabels=False)
+	graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+	return graphJSON
+
+def bubble(labels, n_cluster):
+	lcount = []
+	for i in range (n_cluster):
+		lcount.append(labels.count(i))
+
+	df = pd.DataFrame(np.random.rand(n_cluster, 2))
+	df.columns = ['x', 'y']
+	df['size'] = lcount
+	fig = px.scatter(df, x = 'x', y = 'y', size = 'size', color = 'size', size_max = 50)
+	fig.update_yaxes(visible=False, showticklabels=False)
+	fig.update_xaxes(visible=False, showticklabels=False)
+	graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+	return graphJSON
+
+def empty_plot():
+	fig = px.scatter(pd.DataFrame([[0, 0]]))
+	fig.update_yaxes(visible=False, showticklabels=False)
+	fig.update_xaxes(visible=False, showticklabels=False)
+	graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+	return graphJSON
